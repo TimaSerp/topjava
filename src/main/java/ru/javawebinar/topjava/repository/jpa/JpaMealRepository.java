4be2be3e42e5b9286049276df28d2d145a9fdc99
TimaSerp <timaserp@gmail.com>
2022-06-29 21:26:38 +0300

    Rewrite hw4 with optional

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -2,18 +2,18 @@ package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-@Transactional
+@Transactional(readOnly = true)
 public class JpaMealRepository implements MealRepository {
 
     @PersistenceContext
@@ -22,14 +22,17 @@ public class JpaMealRepository implements MealRepository {
     @Override
     @Transactional
     public Meal save(Meal meal, int userId) {
-        User ref = em.getReference(User.class, userId);
-        meal.setUser(ref);
+        if (meal.getUser() == null) {
+            User ref = em.getReference(User.class, userId);
+            meal.setUser(ref);
+        }
         if (meal.isNew()) {
             em.persist(meal);
             return meal;
-        } else {
+        } else if (meal.getUser().getId() == userId) {
             return em.merge(meal);
         }
+        return null;
     }
 
     @Override
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,7 +1,12 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExternalResource;
+import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,8 +18,11 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertThrows;
+import static org.slf4j.LoggerFactory.getLogger;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
@@ -26,6 +34,37 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
+    private static final Logger log = getLogger(MealServiceTest.class);
+    private static final Map<String, Long> classNamesAndMillis = new HashMap<>();
+    @Rule public TestName name = new TestName();
+
+    @ClassRule
+    public static final ExternalResource externalClassResource = new ExternalResource() {
+        @Override
+        protected void after() {
+            for (Map.Entry<String, Long> entry: classNamesAndMillis.entrySet()) {
+                log.info("test " + entry.getKey() + " took " + entry.getValue() + " milliseconds");
+            }
+        }
+    };
+
+        @Rule
+    public final ExternalResource externalResource = new ExternalResource() {
+        private long start;
+        @Override
+        protected void before() throws Throwable {
+            start = System.currentTimeMillis();
+        }
+
+        @Override
+        protected void after() {
+            long duration = System.currentTimeMillis() - start;
+            classNamesAndMillis.put(name.getMethodName(), duration);
+            log.info("test took " + duration + " milliseconds");
+        }
+
+
+    };
 
     @Autowired
     private MealService service;
